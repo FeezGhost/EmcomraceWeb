607ce24e74ebedd475b725a4de022e6c38aa58e4
feezghost <feezoocrazy420@gmail.com>
2021-02-08 04:03:04 +0500

    part3

--- ecomrace/static/js/cart.js
+++ ecomrace/static/js/cart.js
@@ -1,53 +1,73 @@
-let updateBtns = document.querySelectorAll('.update-cart');
-for(let i = 0 ; i < updateBtns.length ; i++){
-    updateBtns[i].addEventListener('click', () =>{
-        let productId =updateBtns[i].dataset.product;
-        let action = updateBtns[i].dataset.action;
-        
-        if( user === 'AnonymousUser'){
-            console.log('Not Logged in');
-        }
-        else{
-            updateUserOrder(productId,  action);
-        }
-    });
+var updateBtns = document.getElementsByClassName('update-cart')
+var cartItems = document.getElementById('cart-total')
+
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product;
+		var action = this.dataset.action;
+		console.log('productId:', productId, 'Action:', action);
+		console.log('USER:', user);
+
+		if (user == 'AnonymousUser'){
+			addCookieItem(productId, action);
+		}else{
+			updateUserOrder(productId, action);
+		}
+	})
 }
 
-
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-var csrftoken = getCookie('csrftoken');
-
-
-function updateUserOrder ( productId, action){
-    console.log('User : ' ,user, 'is  logged in Sending data');
-    const url = 'update_item/';
-    fetch(url,{
-        method : 'POST',
-        headers : {
-            'Content-Type':'application/json',
-            'X-CSRFToken':csrftoken,
+function updateUserOrder(productId, action){
+    console.log('User is authenticated, sending data...');
+    
+    var url ='/update_item/';
+    $.ajax({
+        url: url,
+        data: {
+          productId: productId,
+          action: action,
         },
-        body:JSON.stringify({'productId': productId, 'action': action}),
-    })
-    .then((response)=>{
-        return response.json();
-    })
-    .then((data)=>{
-        console.log('data: ', data);
-    });
+        dataType: "json",
+    
+        success: function (data) {
+          if (data.is_taken) {
+            alert("A user with this username already exists.");
+          }
+          console.log(data);
+        },
+      });
+      cartItems.innerText = (parseInt(cartItems.innerText) + 1);
 
+      location.reload();
+
+      console.log("productId:", productId, "action:", action);
+      console.log("USER:", user);
+      console.log(" User is logged in , Sending data...");
+    
+    
+}
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	
+	location.reload()
 }
--- ecomrace/store/views.py
+++ ecomrace/store/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from .models import *
+import datetime
 
 # Create your views here.
 def store(request):
     products = Product.objects.all()
-    context = {'products': products}
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        oreder, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = oreder.orderitem_set.all()
+        cartItem = oreder.get_cart_items
+    else:
+        items = []
+        oreder = {'get_cart_items':0, 'get_cart_total':0}
+        cartItem = order['get_cart_items']
+
+    context = {'items': items, 'order': oreder, 'cartItem':cartItem, 'products': products}
     return render(request, 'store/store.html', context)
 
 
@@ -15,11 +27,13 @@ def cart(request):
         customer = request.user.customer
         oreder, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = oreder.orderitem_set.all()
+        cartItem = oreder.get_cart_items
     else:
         items = []
         oreder = {'get_cart_items':0, 'get_cart_total':0}
+        cartItem = order['get_cart_items']
 
-    context = {'items': items, 'order': oreder}
+    context = {'items': items, 'order': oreder, 'cartItem':cartItem}
     return render(request, 'store/cart.html', context)
 
 
@@ -28,17 +42,63 @@ def checkout(request):
         customer = request.user.customer
         oreder, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = oreder.orderitem_set.all()
+        cartItem = oreder.get_cart_items
     else:
         items = []
         oreder = {'get_cart_items':0, 'get_cart_total':0}
+        cartItem = order['get_cart_items']
 
-    context = {'items': items, 'order': oreder}
+    context = {'items': items, 'order': oreder, 'cartItem':cartItem}
     return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
-    data = json.loads(request.body)
-    productId = data['productID']
-    action = data['action']
+    productId = request.GET.get('productId')
+    action = request.GET.get('action')
     print(action)
     print(productId)
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+        
+    orderItem.save()
+    
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    
     return JsonResponse('item was updated', safe=False)
+
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	else:
+		customer, order = guestOrder(request, data)
+
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	if total == order.get_cart_total:
+		order.complete = True
+	order.save()
+
+	if order.shipping == True:
+		ShippingAddress.objects.create(
+		customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],
+		)
+
+	return JsonResponse('Payment submitted..', safe=False)
